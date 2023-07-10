3e9fdb85729059f07b351002d71e34a9aac23f55
alexandrucampan <alexandru.campan@student.upt.ro>
2023-07-10 13:57:57 +0300

    Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,21 @@ package org.example;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Column;
+
 
 public class Main {
     public static void main(String[] args) {
 
         SparkSession sparkSession = SparkSession.builder().master("local").appName("Read_CSV").getOrCreate();
-        Dataset<Row> csv = sparkSession.read().option("header",true).csv("/Users/alexandrucampan/MyCodes/Spark/src/main/resources/erasmus.csv");
-        csv.show();
 
-        csv.printSchema();
+        Dataset<Row> csv = sparkSession.read().option("header",true).csv("/Users/alexandrucampan/MyCodes/Spark/src/main/resources/erasmus.csv");
+
+        csv.show(false);
+
+        csv.groupBy("Receiving Country Code","Sending Country Code")
+                    .count()
+                    .orderBy(new Column("Receiving Country Code"))
+                    .show(40);
     }
 }
